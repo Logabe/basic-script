8d1477bd8823e68e418343c355a7327a3d222d48
Logabe <31550575+Logabe@users.noreply.github.com>
2018-03-10 09:44:17 +1300

    Update XY.js

--- src/motion/XY.js
+++ src/motion/XY.js
@@ -2,18 +2,18 @@ function setX (sprite, x){
  var els = document.getElementsByClassName(sprite);
  for (var i = 0; i < els.length; i++) {
   document.els.style.position = "fixed";
-  document.els.style.right = x + "px";    
+  document.els.style.right = x;    
 }}
 function setY (sprite, y){
  var els = document.getElementsByClassName(sprite);
  for (var i = 0; i < els.length; i++) {
   document.els.style.position = "fixed";
-  document.els.style.bottom = y + "px";    
+  document.els.style.bottom = y;    
 }}
 function setPosition (sprite, x, y){
  var els = document.getElementsByClassName(sprite);
  for (var i = 0; i < els.length; i++) {
   document.els.style.position = "fixed";
-  document.els.style.right = x + "px";
-  document.els.style.right = y + "px";
+  document.els.style.right = x;
+  document.els.style.right = y;
 }}
