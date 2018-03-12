80402508b6673e140ae69fe4b71c0e8cbaeb7adb
Logabe <31550575+Logabe@users.noreply.github.com>
2018-03-13 07:25:02 +1300

    Create getXY.js

---
+++ src/sensing/getXY.js
@@ -0,0 +1,6 @@
+function getX (sprite){
+ return document.getElementsByClassName(sprite).style.left + "px";
+}
+function getY (sprite){
+ return document.getElementsByClassName(sprite).style.bottom + "px";
+}
